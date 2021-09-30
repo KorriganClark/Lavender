f8f8aa5bab4cd29f7db3f92d95f8ed836bdc87e3
Yamai <852515440@qq.com>
2021-09-30 19:12:20 +0800

    skilleditor

---
+++ Lavender/Assets/Editor/SkillEditor/Skills/AttackTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Lavender;
+using Lavender.Cardinal;
+using UnityEngine;
+
+public class AttackTest : ISkillScript
+{
+    private string skillName = "AttackTest";
+    public string SkillName
+    {
+        get
+        {
+            return skillName;
+        }
+    }
+    private SkillPlayableAsset skillPlayable;
+    public SkillPlayableAsset SkillAsset { get { return skillPlayable; } set => skillPlayable = value; }
+    private float skillTime = 2;
+    public float SkillTime
+    {
+        get
+        {
+            return skillTime;
+        }
+        set
+        {
+            skillTime = value;
+        }
+    }
+    public int ChannelNum
+    {
+        get
+        {
+            return Channels.Count;
+        }
+    }
+
+    public List<string> Channels
+    {
+        get
+        {
+            return new List<string>();
+        }
+    }
+
+    public void OnSkillBegin()
+    {
+
+    }
+
+    public void OnSkillStop()
+    {
+       
+    }
+
+    public void OnSkillSucceed()
+    {
+        
+    }
+}
